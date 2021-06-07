538b913a5cd916803f52ed76bf59a4e6c0507de7
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-06-07 16:19:18 +0200

    add DB connection

---
+++ Database/connect.php
@@ -0,0 +1,7 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=oop_pokemon";
+$user = "root";
+$passwd = "test";
+
+?>
--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=oop_pokemon";
-$user = "root";
-$passwd = "test";
+include "Database/connect.php";
 
 $pdo = new PDO($dsn, $user, $passwd);
 
 $stm = $pdo->query("SELECT VERSION()");
 
 $version = $stm->fetch();
-echo "coucou";
+
 echo $version[0] . PHP_EOL;
