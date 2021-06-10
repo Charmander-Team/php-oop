7049bcb4a3dc515d4ecf4ab7b826f36a11f6d723
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-06-10 12:06:30 +0200

    remove Database.php from .gitignore

---
+++ Core/DB/Database.php
@@ -0,0 +1,29 @@
+<?php
+namespace Core\DB;
+
+class Database {
+
+    private $dbUser = "root";
+    private $dbPass = "test";
+    private $host = "localhost";
+    private $dbName = "oop_pokemon";
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = new \PDO("mysql:host=$this->host;dbname=$this->dbName", $this->dbUser, $this->dbPass, [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            // Use for display special character on view 
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+        ]);
+    }
+
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+}
+
+$this->pdo = new \PDO("mysql:host=$this->host;dbname=$this->dbName", $this->dbUser, $this->dbPass, [
+    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+]);
