23d84223bae17d92c666d9b5280014a4b8295e39
Witkowski Antoine <witkowski.antoine@gmail.com>
2021-06-10 18:24:51 +0200

    update .env

--- Core/DB/Database.php
+++ Core/DB/Database.php
@@ -5,17 +5,18 @@ class Database
 {
     //private $dbUser = "root";
     //private $dbPass = "test";
-    private $host = "localhost";
+    //private $host = "localhost";
     //private $dbName = "oop_pokemon";
     private $pdo;
 
     public function __construct()
     {
+        $dbHost = $_ENV['MYSQL_HOST'];
         $dbUser = $_ENV['MYSQL_USER'];
         $dbPassword = $_ENV['MYSQL_PASSWORD'];
         $dbName = $_ENV['MYSQL_DATABASE'];
 
-        $this->pdo = new \PDO("mysql:host=$this->host;dbname=$dbName", $dbUser, $dbPassword, [
+        $this->pdo = new \PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword, [
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             // Use for display special character on view
             \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
