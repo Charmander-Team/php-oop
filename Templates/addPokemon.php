4083e5d58d820930436cf0f82ce6a00ed111b345
yugo940 <kahteran.dino@hotmail.com>
2021-06-10 17:58:40 +0200

    adding navbar and form

--- Templates/addPokemon.php
+++ Templates/addPokemon.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+<div style="background: no-repeat url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/d9e6161a-962f-4986-a8e2-5ae6a53fd317/d4n85cg-2352b9f6-4f2e-4e93-a9da-7086574b37b7.png');height:100vh">
+<form class="container text-center form" style="padding-top: 175px;color:white" method="POST">
+  <div class="row mb-4">
+    <div class="col">
+      <div class="form-outline">
+      <label for="name" class="form-label">Name:</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Nom du pokémon">
+      </div>
+    </div>
+    <div class="col">
+      <div class="form-outline">
+      <label for="location" class="form-label">Location:</label>
+            <input type="text" class="form-control" name="location" id="location" placeholder="Lieu du pokémon">
+      </div>
+    </div>
+  </div>
+  <div class="form-outline mb-4">
+  <label for="hp" class="form-label">HP:</label>
+            <input type="number" class="form-control" name="hp" id="hp" min="0" value="0" step="10">
+  </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-
-<body>
-    <form class="form" method="POST">
-        <div class="form-group">
-            <label for="name" class="form-label">Name:</label>
-            <input type="text" class="form-control" name="name" id="name" placeholder="pikachu">
-        </div>
-        <div class="form-group">
-            <label for="location" class="form-label">Location:</label>
-            <input type="text" class="form-control" name="location" id="location" placeholder="kanto">
-        </div>
-        <div class="form-group">
+  <div class="form-group">
             <label for="type" class="form-label">Type:</label>
             <select name="type" id="type">
                 <option value="" disabled>--Select type--</option>
@@ -29,10 +29,6 @@
             </select>
         </div>
         <div class="form-group">
-            <label for="hp" class="form-label">HP:</label>
-            <input type="number" class="form-control" name="hp" id="hp" min="0" value="0" step="10">
-        </div>
-        <div class="form-group">
             <label for="hasEvolve" class="form-label">HasEvolve:</label>
             <input type="checkbox" class="form-control" name="hasEvolve" id="hasEvolve" >
         </div>
@@ -40,8 +36,7 @@
             <label for="image" class="form-label">Image URL (.png):</label>
             <input type="url" class="form-control" name="image" id="image" pattern="https://.*.png" placeholder="https://example.png">
         </div>
-        <button class="btn btn-primary">Submit</button>
-    </form>
-</body>
-
-</html>
+  <!-- Submit button -->
+  <button type="submit" class="btn btn-primary btn-block mb-4">Créer</button>
+</form>
+</div>
---
+++ Templates/navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="http://localhost/php-oop/public/index.php/index.php?page=home"><img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png" width="50px"></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link pt-4" href="http://localhost/php-oop/public/index.php/index.php?page=addPokemon">Ajouter un pokémon</a>
+    </li>
+  </ul>
+</nav>
